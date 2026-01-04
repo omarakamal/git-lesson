de470298a104a141671d42fe03ff98a8f2f6fa68
omarakamal <omaraakamal@gmail.com>
2026-01-04 09:30:20 +0300

    johns changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <h2>THIS IS MY FOOTER</h2>
+        <p>IT IS GREAT</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <ul>
+            <li>HomePage</li>
+            <li>About Us</li>
+            <li>Donate to Us</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
